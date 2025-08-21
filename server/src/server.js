c292604cf729a0de0372e23df22290014de989e9
dinesh725 <dineshkhatua672@gmail.com>
2025-08-21 13:02:35 +0530

    Added dotenv config at top of server.js

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,9 @@
 // require("dotenv").config()
+
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 const app = require("./app")
 const logger = require("./config/logger")
 
